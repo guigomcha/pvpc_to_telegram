e8487c4ea7aa119f5f35c155d2e364c4282dc43e
Guillelerial <guillermo.gc1994@gmail.com>
2020-08-20 17:53:09 +0200

    tested NGSI-v2 model

--- pvpcservice/controllers/api.py
+++ pvpcservice/controllers/api.py
@@ -12,23 +12,9 @@ def telegram(access_info):
         return f"Ups! There was a problem. {e}", HTTPStatus.INTERNAL_SERVER_ERROR
 
 
-def prices():
+def prices_v2():
     try:
-        df = PVPC().get_pvpc_df()
-        result = df.to_json(orient="columns")
-        parsed = json.loads(result)
-        response = json.dumps(parsed, indent=4)
+        response = PVPC().get_ngsi_v2_model()
         return f"{response}", HTTPStatus.OK
     except Exception as e:
         return f"Ups! There was a problem. {e}", HTTPStatus.INTERNAL_SERVER_ERROR
-
-
-def prices_v2():
-    pass
-
-# if __name__ == "__main__":
-#     access_info = {
-#       "bot_token": "1361411284:AAHmT2mlcpAW9RNiZ3D7fe8rgwsdH_W-5xY",
-#       "chats_token": ["-438788913"]
-#     }
-#     telegram(access_info)
---
+++ pvpcservice/ngsi/entities.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+
+def get_ngsi_v2_entity(df, id, first_ts, source="http://www.aemet.es/xml/municipios/localidad_46250.xml", timezone="Europe/Madrid", unit="EUR"):
+    df['utc_iso_format'] = df['ts_end'].apply(pd.Timestamp.isoformat)
+    entity_v2 = {
+        "id": f"urn:ngsi-ld:Forecast:{id}",
+        "type": "Forecast",
+        "category": "EnergyPrice",
+        "ObservedAt": {
+            "value": pd.Timestamp.utcnow().tz_convert(timezone).isoformat()
+        },
+        "dateIssued": {
+            "value": first_ts
+        },
+        "source": {
+            "value": source
+        },
+        "timezone": {
+            "value": timezone
+        },
+        "indexes": {
+            "value": df['utc_iso_format'].to_list(),
+            "metadata": {
+                "unitCode": {
+                    "value": "ISO8601"
+                }
+            }
+        },
+        "values": {
+            "value": df['price'].to_list(),
+            "metadata": {
+                "unitCode": {
+                    "value": unit
+                }
+            }
+        }
+    }
+    return entity_v2
