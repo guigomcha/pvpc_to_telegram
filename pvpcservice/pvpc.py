33349e566ffcb59eedf684a86fe7b6ac8778816e
Guillelerial <guillermo.gc1994@gmail.com>
2020-08-20 17:52:03 +0200

    refactoring

--- pvpcservice/pvpc.py
+++ pvpcservice/pvpc.py
@@ -1,27 +1,74 @@
 import bs4
+import logging
+import matplotlib.pyplot as plt
 import pandas as pd
 import requests
 
+from loggingbot.loggingbot import TelegramBotHandler
+
+from pvpcservice.ngsi import entities
 from pvpcservice import utils
 
 
 class PVPC:
-    @staticmethod
-    def _get_pvpc():
+    def __init__(self):
+        self.entity_id = "EnergyPricePVPC1"
+        self.source = 'https://tarifaluzhora.es/'
+        self.tz = "Europe/Madrid"
+
+    @property
+    def today(self):
+        return pd.Timestamp.utcnow().tz_convert(self.tz).floor('d')
+
+    def _get_pvpc_df(self):
         # Download page
-        date_observed = pd.Timestamp.utcnow().tz_convert("Europe/Madrid").floor('d')  # TODO: Query date
-        web = requests.get('https://tarifaluzhora.es/')
+        date_issued = self.today  # TODO: Query date
+        web = requests.get(self.source)
         web.raise_for_status()  # if error it will stop the program
         content = bs4.BeautifulSoup(web.text, 'html.parser')
         interval_to_price = utils.scrap_price_section(content)
-        return utils.build_df(date_observed, interval_to_price)
+        return utils.build_df(date_issued, interval_to_price)
 
     def alert_telegram(self, bot_token, chats_token):
-        df = self._get_pvpc()
-        utils.send_fig_to_telegram(df, bot_token, chats_token)
+        """
+        Creates a plot for Time Interval to Price value and sends it to Telegram
 
-    def get_pvpc_df(self):
-        return self._get_pvpc()
+        Args:
+            bot_token (string): Telegram Bot token id
+            chats_token (list of string): Telegram chat ids
 
-    def get_ngsi_model(self):
-        pass
+        """
+        df = self._get_pvpc_df()
+        # TODO: parametrise threshold for 'cheap' energy
+        thr = 0.1
+        logging.root.setLevel(logging.INFO)
+        tbh = TelegramBotHandler(bot_token, chats_token)
+        logging.root.addHandler(tbh)
+        logger = logging.getLogger('pvpc_bot')
+        today = df['ts_start'][0].date()
+        degrees = 90
+        # Format data and plot
+        df_cheap = df[df['price'] < thr]
+        df_expensive = df[df['price'] >= thr]
+
+        plt.bar(df_cheap['interval'], df_cheap['price'], color='g')
+        plt.bar(df_expensive['interval'], df_expensive['price'], color='r')
+        plt.ylim(df['price'].min(), df['price'].max())
+        plt.title(f'PVPC prices {today} in Spain')
+        plt.xticks(rotation=degrees)
+        plt.xlabel('Time Interval Applicable')
+        plt.ylabel('Price (€)')
+        plt.grid(True, axis='both')
+
+        # Send the figure to Telegram
+        logger.info(f'Extracting PVPC for energy in Spain for {today}', extra={'bot': True, 'figure': plt.gcf()})
+
+    def get_ngsi_v2_model(self):
+        """
+        Obtains forecasts and returns back an NGSI-v2 complaint entity
+        Returns:
+
+        """
+        df = self._get_pvpc_df()
+        entity = entities.get_ngsi_v2_entity(df, self.entity_id, self.today)
+        return entity
--- pvpcservice/utils.py
+++ pvpcservice/utils.py
@@ -1,9 +1,4 @@
-import logging
-import matplotlib.pyplot as plt
 import pandas as pd
-
-
-from loggingbot.loggingbot import TelegramBotHandler
 
 
 def scrap_price_section(content):
@@ -28,12 +23,12 @@ def _format_timestamp(ts):
     return ts.tz_localize(None)
 
 
-def build_df(date_observed, interval_to_price):
+def build_df(dte_issued, interval_to_price):
     """
     Create a df based on scrapped values for further any
 
     Args:
-        date_observed(pd.Timestamp): Local time Z-aware
+        dte_issued(pd.Timestamp): Local time Z-aware
         interval_to_price (dict, {str: float}): Time interval to price value pairs. e.g {'00h - 01h': 0.1005}
 
     Returns (pd.DataFrame):
@@ -42,44 +37,9 @@ def build_df(date_observed, interval_to_price):
     data = []
     for interval, price in interval_to_price.items():
         data.append({
-            'ts_start': _format_timestamp(date_observed + pd.Timedelta(interval.split(' ')[0])),
-            'ts_end': _format_timestamp(date_observed + pd.Timedelta(interval.split(' ')[-1])),
+            'ts_start': _format_timestamp(dte_issued + pd.Timedelta(interval.split(' ')[0])),
+            'ts_end': _format_timestamp(dte_issued + pd.Timedelta(interval.split(' ')[-1])),
             'interval': interval,
             'price': price
         })
     return pd.DataFrame(data)
-
-
-def send_fig_to_telegram(df, bot_token, chats_token):
-    """
-    Creates a plot for Time Interval to Price value and sends it to Telegram
-
-    Args:
-        df (pd.DataFrame): Expects ts and price columns
-        bot_token (string): Telegram Bot token id
-        chats_token (list of string): Telegram chat ids
-
-    """
-    # TODO: parametrise threshold for 'cheap' energy
-    thr = 0.1
-    logging.root.setLevel(logging.INFO)
-    tbh = TelegramBotHandler(bot_token, chats_token)
-    logging.root.addHandler(tbh)
-    logger = logging.getLogger('pvpc_bot')
-    today = df['ts_start'][0].date()
-    degrees = 90
-    # Format data and plot
-    df_cheap = df[df['price'] < thr]
-    df_expensive = df[df['price'] >= thr]
-
-    plt.bar(df_cheap['interval'], df_cheap['price'], color='g')
-    plt.bar(df_expensive['interval'], df_expensive['price'], color='r')
-    plt.ylim(df['price'].min(), df['price'].max())
-    plt.title(f'PVPC prices {today} in Spain')
-    plt.xticks(rotation=degrees)
-    plt.xlabel('Time Interval Applicable')
-    plt.ylabel('Price (€)')
-    plt.grid(True, axis='both')
-
-    # Send the figure to Telegram
-    logger.info(f'Extracting PVPC for energy in Spain for {today}', extra={'bot': True, 'figure': plt.gcf()})
