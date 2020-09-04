29ecaa37ddab4ac5f463fe75249fef6956ac7b57
Guillelerial <guillermo.gc1994@gmail.com>
2020-09-04 13:46:56 +0200

    refactored models

--- pvpcservice/ngsi/entities.py
+++ pvpcservice/ngsi/entities.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
 
-def get_ngsi_v2_entity(df, id, first_ts, source="http://www.aemet.es/xml/municipios/localidad_46250.xml", timezone="Europe/Madrid", unit="EUR"):
+def get_ngsi_v2_entity(df, id, first_ts, provider="http://www.aemet.es/xml/municipios/localidad_46250.xml",
+                       timezone="Europe/Madrid", unit="EUR/kWh", category="Deterministic"):
     """
     Provides a NGSI-v2 entity
 
@@ -9,45 +10,37 @@ def get_ngsi_v2_entity(df, id, first_ts, source="http://www.aemet.es/xml/municip
         df (pd.Dataframe): Expects columns ts_end and price describing a forecast
         id (str): Id of the entity. It will be appended to  the prefix "urn:ngsi-ld:Forecast:"
         first_ts (pd.Timestamp): Localtime z-aware ts describing the first timestamp from this the forecast applies
-        source (string): URl of the site that provided the forecast
+        provider (string): URl of the site that provided the forecast
         timezone (string): Region identification for the timezone
         unit (string): UnitCode for the forecast values
+        category (string): Type of forecast ["Stochastic", "Deterministic", "Generic", "Others"]
 
     Returns (dict):
         NGSI-v2 entity
     """
+    # TODO: Update to a Price Entity that is updated 24 time when the new prices are available
     df['utc_iso_format'] = df['ts_end'].apply(pd.Timestamp.isoformat)
     entity_v2 = {
         "id": f"urn:ngsi-ld:Forecast:{id}",
         "type": "Forecast",
-        "category": "EnergyPrice",
-        "ObservedAt": {
-            "value": pd.Timestamp.utcnow().tz_convert(timezone).floor('s').isoformat()
-        },
+        "category": category,
         "dateIssued": {
             "value": first_ts.isoformat()
         },
-        "source": {
-            "value": source
+        "provider": {
+            "value": provider
         },
         "timezone": {
             "value": timezone
         },
         "indexes": {
-            "value": df['utc_iso_format'].to_list(),
-            "metadata": {
-                "unitCode": {
-                    "value": "ISO8601"
-                }
-            }
+            "value": df['utc_iso_format'].to_list()
         },
         "values": {
-            "value": df['price'].to_list(),
-            "metadata": {
-                "unitCode": {
-                    "value": unit
-                }
-            }
+            "value": df['price'].to_list()
+        },
+        "unitCode": {
+            "value": unit
         }
     }
     return entity_v2
