5f01cfe1e184f0fde043726fea862622784d606a
Guillelerial <guillermo.gc1994@gmail.com>
2020-09-13 14:19:50 +0200

    refactoring

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,4 @@ Hostport=8080
 Localport=8080
 Network=bridge
 
-docker run  --name pvpc_service -p $Hostport:$Localport pvpc_service
-docker network connect $Network pvpc_service
+docker run  --network=$Network --name pvpc_service -p $Hostport:$Localport pvpc_service
