6afbdb26a007e6664aa2f6c8b0998b926eb80fcc
Guillelerial <guillermo.gc1994@gmail.com>
2020-09-13 12:31:00 +0200

    Refactoring. Split docker run into build and deploy in order to combine docker solo runs and docker compose scenarios

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,6 @@
 Hostport=8080
 Localport=8080
 Network=bridge
-docker rm $(docker stop $(docker ps -a -q --filter ancestor=pvpc_service --format="{{.ID}}"))
-docker container rm pvpc_service
 
-rm -Rf loggingbot
-git clone https://github.com/Guillelerial/loggingbot.git
-
-docker build --no-cache -t pvpc_service .
-docker run --network=$Network --name pvpc_service -p $Hostport:$Localport pvpc_service
+docker run  --name pvpc_service -p $Hostport:$Localport pvpc_service
+docker network connect $Network pvpc_service
